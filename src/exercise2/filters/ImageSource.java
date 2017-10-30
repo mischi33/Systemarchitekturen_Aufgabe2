a421e8ef5be1b076e921942836d54415dcff34aa
Michelle <michelle.weber@students.fhv.at>
2017-10-30 11:14:22 +0100

    initial commit

---
+++ src/exercise2/filters/ImageSource.java
@@ -0,0 +1,15 @@
+package exercise2.filters;
+
+import pmp.filter.Source;
+
+import java.io.StreamCorruptedException;
+
+/**
+ * Created by Michelle on 30.10.2017.
+ */
+public class ImageSource extends Source<String> {
+    @Override
+    public String read() throws StreamCorruptedException {
+        return null;
+    }
+}
