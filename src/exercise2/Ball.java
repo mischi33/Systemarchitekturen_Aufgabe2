d9013c03b299cb7bcc1f98147abad08ec6b31b77
Flowkii <florian.braun@students.fhv.at>
2017-11-12 18:14:33 +0100

    added some more balls

--- src/exercise2/Ball.java
+++ src/exercise2/Ball.java
@@ -1,5 +1,6 @@
 package exercise2;
 
+import com.sun.istack.internal.Nullable;
 import exercise2.filters.calcCentroidsFilter.Coordinate;
 
 public class Ball {
@@ -39,5 +40,23 @@ public class Ball {
 
     public void setCoordToleranceOk(boolean coordToleranceOk) {
         this.coordToleranceOk = coordToleranceOk;
+    }
+
+    @Override
+    public String toString(){
+        String data="Centroid Coordinates:\nx = "+coordinate._x + "\ny = "+coordinate._y + "\n\n"+"Diameter = " + diameter +"\n\n";
+        if (coordToleranceOk && diameterToleranceOk){
+            return data+ "Coordinate is in Tolerance\nDiameter is in Tolerance";
+
+        }
+        if (coordToleranceOk && !diameterToleranceOk){
+            return data+ "Coordinate is in Tolerance\nDiameter is not in Tolerance";
+
+        }
+        if (!coordToleranceOk && diameterToleranceOk){
+            return data+ "Coordinate is not in Tolerance\nDiameter is in Tolerance";
+
+        }
+            return data+ "Coordinate is not in Tolerance\nDiameter is not in Tolerance";
     }
 }
